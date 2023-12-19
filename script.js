0ddd31a89321abbb355fb0af78ea596a07d078bf
Shroomy Ralsei <142345364+ShroomyRalsei@users.noreply.github.com>
2023-12-19 10:32:26 -0400

    feat:Add grid creator function

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
 const gridContainer = document.querySelector('#grid-container');
 
 const gridCreator = document.querySelector('#grid-creator');
+
+function createGrid(squares) {
+    const grid = document.createElement('div');
+
+    grid.setAttribute('id','grid');
+
+    gridContainer.appendChild(grid);
+
+    for (i = 0; i < squares; i++) {
+        const horizontalSquare = document.createElement('div');
+
+        horizontalSquare.setAttribute('id', 'horizontal-square')
+
+        grid.appendChild(horizontalSquare);
+
+        for (x = 0; x < squares; x++) {
+            const verticalSquare = document.createElement('div');
+
+            verticalSquare.setAttribute('id', 'vertical-square')
+
+            horizontalSquare.appendChild(verticalSquare);
+        }
+    }
+}
